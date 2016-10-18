dfc6e98b325e27e2423e8005135a311b99e2ecd7
= <Phi@gold.ratio>
2016-10-18 01:17:18 +0000

    Updated Readme

--- README.m
+++ README.m
@@ -2,4 +2,6 @@
 A small programming language written in nim named Intersect. The syntax is in Greek. <br>
 The goal of this language is to write a small, fast, expandable language using a syntax based upon Greek words. <br>
 The parser generates a pseudo AST which severely compresses the original code and allows for easier parsing. <br>
-Example Intersect code is in : ```intersect.sct```
+Example Intersect code is in : ```Nim
+intersect.sct
+```
